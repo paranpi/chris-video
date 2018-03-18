da6129b3037c5447ddab9fc4d9796dc5035913d6
paranpi <paranpi666@gmail.com>
2018-03-18 21:59:18 +0900

    preload false

--- application/views/layout/content.php
+++ application/views/layout/content.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		<?php foreach($file_list as $value) { ?>
 			<div class="col-md-4">
-				<video class="video-js vjs-default-skin vjs-16-9" controls preload="false" data-setup="{}">
+				<video class="video-js vjs-default-skin vjs-16-9" controls preload="none" data-setup="{}">
 					<source src="/<?php echo $value['base'].'/'.$value['path'].'/'.$value["name"]?>" type="video/mp4">
 					<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that
 						<a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
